506703a5ad53530e643ec03cfe6ecb6f00cd9a28
thinhlh267 <hoangthinhdz1999@gmail.com>
2026-07-16 13:19:03 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,35 +10,19 @@ use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -48,12 +31,8 @@ class User extends Authenticatable implements FilamentUser
         ];
     }
 
-    /**
-     * Quyết định ai có quyền vào Admin Panel
-     */
     public function canAccessPanel(Panel $panel): bool
     {
-        // Cho phép tài khoản có email này được vào hệ thống quản trị
-        return $this->email === 'lehoangthinh2233@gmail.com';
+        return true; 
     }
 }
