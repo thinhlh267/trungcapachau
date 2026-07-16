4ce1e4e55b1fa834dec47a1e2da4213618befea0
thinhlh267 <hoangthinhdz1999@gmail.com>
2026-07-16 14:24:37 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Ép Laravel tin tưởng toàn bộ Proxies của Railway để xử lý HTTPS đồng bộ
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
